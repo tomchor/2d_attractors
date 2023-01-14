31c005c664995319f84a2307124b3345f92fbdcc
tomchor <tomaschor@gmail.com>
2023-01-14 07:57:22 -0800

    include python script, jupyter notebook and yml file

---
+++ attract.py
@@ -0,0 +1,129 @@
+import numpy as np, pandas as pd, datashader as ds
+from datashader import transfer_functions as tf
+from datashader.colors import inferno, viridis
+from datashader.utils import export_image
+from numba import jit
+from math import sin, cos, sqrt, fabs
+
+@jit(nopython=True)
+def Clifford(x, y, a, b, c, d, *o):
+    return sin(a * y) + c * cos(a * x), \
+           sin(b * x) + d * cos(b * y)
+
+@jit(nopython=True)
+def Chor1(x, y, a, b, c, d, *o):
+    return sin(a * y)**2 + c * cos(a * x), \
+           sin(b * x) + d * cos(b * y)**2
+
+@jit(nopython=True)
+def Chor2(x, y, a, b, c, d, *o):
+    return sin(a * y) + c * cos(a * x), \
+           d * cos(b * x) + sin(b * y)
+
+@jit(nopython=True)
+def De_Jong(x, y, a, b, c, d, *o):
+    return sin(a * y) - cos(b * x), \
+           sin(c * x) - cos(d * y)
+
+@jit(nopython=True)
+def Svensson(x, y, a, b, c, d, *o):
+    return d * sin(a * x) - sin(b * y), \
+           c * cos(a * x) + cos(b * y)
+
+@jit(nopython=True)
+def Bedhead(x, y, a, b, *o):
+    return sin(x*y/b)*y + cos(a*x-y), \
+           x + sin(y)/b
+
+@jit(nopython=True)
+def Fractal_Dream(x, y, a, b, c, d, *o):
+    return sin(y*b)+c*sin(x*b), \
+           sin(x*a)+d*sin(y*a)
+
+@jit(nopython=True)
+def Hopalong1(x, y, a, b, c, *o):
+    return y - sqrt(fabs(b * x - c)) * np.sign(x), \
+           a - x
+
+@jit(nopython=True)
+def Hopalong2(x, y, a, b, c, *o):
+    return y - 1.0 - sqrt(fabs(b * x - 1.0 - c)) * np.sign(x - 1.0), \
+           a - x - 1.0
+
+@jit(nopython=True)
+def G(x, mu):
+    return mu * x + 2 * (1 - mu) * x**2 / (1.0 + x**2)
+
+@jit(nopython=True)
+def Gumowski_Mira(x, y, a, b, mu, *o):
+    xn = y + a*(1 - b*y**2)*y  +  G(x, mu)
+    yn = -x + G(xn, mu)
+    return xn, yn
+
+@jit(nopython=True)
+def Symmetric_Icon(x, y, a, b, g, om, l, d, *o):
+    zzbar = x*x + y*y
+    p = a*zzbar + l
+    zreal, zimag = x, y
+    
+    for i in range(1, d-1):
+        za, zb = zreal * x - zimag * y, zimag * x + zreal * y
+        zreal, zimag = za, zb
+    
+    zn = x*zreal - y*zimag
+    p += b*zn
+    
+    return p*x + g*zreal - om*y, \
+           p*y - g*zimag + om*x
+
+
+
+n=int(1e7)
+
+@jit(nopython=True)
+def trajectory_coords(fn, x0, y0, a, b=0, c=0, d=0, e=0, f=0, n=n):
+    x, y = np.zeros(n), np.zeros(n)
+    x[0], y[0] = x0, y0
+    for i in np.arange(n-1):
+        x[i+1], y[i+1] = fn(x[i], y[i], a, b, c, d, e, f)
+    return x,y
+
+def trajectory(fn, x0, y0, a, b=0, c=0, d=0, e=0, f=0, n=n):
+    x, y = trajectory_coords(fn, x0, y0, a, b, c, d, e, f, n)
+    return pd.DataFrame(dict(x=x,y=y))
+
+
+def dsplot(fn, vals, n=n, cmap=viridis, label=True):
+    """Return a Datashader image by collecting `n` trajectory points for the given attractor `fn`"""
+    lab = ("{}, "*(len(vals)-1)+" {}").format(*vals) if label else None
+    df  = trajectory(fn, *vals, n=n)
+    cvs = ds.Canvas(plot_width = 700, plot_height = 700)
+    agg = cvs.points(df, 'x', 'y')
+    img = tf.shade(agg, cmap=cmap, name=lab)
+    return img
+
+from colorcet import palette
+palette["viridis"]=viridis
+palette["inferno"]=inferno
+
+import yaml
+vals = yaml.load(open("strange_attractors.yml","r"), Loader=yaml.FullLoader)
+
+def args(name):
+    """Return a list of available argument lists for the given type of attractor"""
+    return [v[1:] for v in vals if v[0]==name]
+
+def plot(fn, vals=None, **kw):
+    """Plot the given attractor `fn` once per provided set of arguments."""
+    vargs=args(fn.__name__) if vals is None else vals
+    return tf.Images(*[dsplot(fn, v[1:], cmap=palette[v[0]][::-1], **kw) for v in vargs]).cols(4)
+
+for attr_name in 
+attr_name = "Chor1"
+print(f"Plotting images using {attr_name} attractor")
+imgs = plot(eval(attr_name))
+for i, img in enumerate(imgs.images):
+    print(i)
+    export_image(img=img, filename=f'{attr_name}_{i}', fmt=".png",  export_path=".",
+                 background="#FFF4CA")
+
